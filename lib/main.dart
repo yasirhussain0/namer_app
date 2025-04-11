ab6657b4bba432e95812057474b51289bd5c6ddb
yasirhussain0 <yasipkuk115@gmail.com>
2025-04-11 23:34:17 +0500

    added Text-Theme

--- lib/main.dart
+++ lib/main.dart
@@ -65,14 +65,22 @@ class BigCard extends StatelessWidget {
 
   final WordPair pair;
 
+ // ...
+
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
+    final theme = Theme.of(context);
+    // ↓ Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
-      color: theme.colorScheme.primaryContainer,
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        // ↓ Change this line.
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
